17412ac436dca7ef35914851fe9e3be86bdd662d
Ruta Joshi <rjoshi@berkeley.edu>
2019-10-19 15:52:16 -0700

    Started working on logistic regression in the ipynb. Will migrate to the python script when it makes sense.

---
+++ logistic_regression.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+import pandas as pd
+
+%matplotlib inline
+
+### This script runs logistic regression on the input data to see if it is learnable. ###
+
+# labels = genfromtxt('labels.csv', delimiter=',')
